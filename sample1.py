da7edf793fcbb5a4000a42de8454c9a3d65432be
Masao Taketani <taket003@umn.edu>
2018-07-26 20:01:03 +0900

    sample1.py is modified

--- sample1.py
+++ sample1.py
@@ -20,6 +20,7 @@ o.start()
 latest_update = datetime.datetime.now()
 while True:
     data = o.getLatestData(uId)
+    vars(data)
     if data is not None:
 
         if data.tick_last_update > latest_update:
